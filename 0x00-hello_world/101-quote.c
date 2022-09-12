d9012907a84385278fb84aa45f51047b7e3c85b8
Francis Onukwu <onukwuf@gmail.com>
2022-09-12 03:50:28 -0700

    Added new C program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Entry point for the project
+*
+* Return: Intentionally made it a 1, to see the error
+*/
+int main(){
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return 1;
+}
