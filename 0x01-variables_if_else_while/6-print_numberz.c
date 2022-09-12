34b5826a1e1e0087d6fb8a3259a6dcc7f904f848
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-13 00:01:22 +0100

    Added New C Code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry Function
+*
+* Return: It always returns success as zero
+*/
+int main(void)
+{
+int i;
+i = 48;
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+
+putchar('\n');
+return (0);
+}
