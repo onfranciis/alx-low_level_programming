6082589e90cf55331edf51d828d134ccd5d536e5
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-12 23:52:15 +0100

    Added New C File

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry Function
+*
+* Return: It always returns success as zero
+*/
+int main(void)
+{
+for (int i = 0; i < 10; i++)
+{
+printf("%d",i);
+}
+printf("\n");
+return (0);
+}
