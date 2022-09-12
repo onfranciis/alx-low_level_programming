8c67a7eb2ba13cd692f220b7fdcb362abab9c49a
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-12 23:43:41 +0100

    Added New C File

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - Entry Function
+*
+* Return: It always returns success as zero
+*/
+int main(void)
+{
+char alphabet;
+
+alphabet = 'a';
+while (alphabet <= 'z')
+{
+if((alphabet != 'q') && (alphabet !=  'e'))
+{
+putchar(alphabet);
+alphabet++;
+}
+else
+{
+alphabet++;
+}
+}
+
+putchar('\n');
+return (0);
+}
