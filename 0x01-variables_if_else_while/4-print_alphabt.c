f3c69b56e0970595141ac5f52d98793ed59b4613
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-12 23:46:33 +0100

    Betty Adjusted

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ char alphabet;
 alphabet = 'a';
 while (alphabet <= 'z')
 {
-if((alphabet != 'q') && (alphabet !=  'e'))
+if ((alphabet != 'q') && (alphabet !=  'e'))
 {
 putchar(alphabet);
 alphabet++;
