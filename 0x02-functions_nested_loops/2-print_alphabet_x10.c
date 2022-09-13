2350aeed6efa5aab74247b22eeafffe27b5a6a8c
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-14 00:14:33 +0100

    Added New Code

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Entry Function
+*
+* Return: It always returns success as zero
+*/
+
+void print_alphabet_x10(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
