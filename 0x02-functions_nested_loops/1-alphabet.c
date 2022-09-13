a3a4f0c959aad9a318fcec1ded10d52b528f3f49
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-13 23:03:20 +0100

    Added a function that prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main - Entry Function
+*
+* Return: It always returns success as zero
+*/
+
+void print_alphabet(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+// _putchar('\n');
+}
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
