22eab81ff4a788bd4da5bffdf34f0b95f67707f1
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-14 00:54:31 +0100

    Betty Styled

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
-* print_alphabet_x10 - Entry Function
-* @c: The value to decide on
 * _islower - Returns the decision
+* @c: The value to decide on
 *
 * Return: It always returns success as zero
 */
