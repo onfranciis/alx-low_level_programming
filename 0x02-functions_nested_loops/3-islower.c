6bb62390efc1c3285cac2856d317913865e20dce
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-14 00:48:00 +0100

    Added Code

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Entry Function
+*
+* Return: It always returns success as zero
+*/
+
+int _islower(int c)
+{
+if (c > 96)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
