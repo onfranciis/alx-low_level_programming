66fe4b41032b148de2c842a41b0c316f82d04538
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-14 01:13:23 +0100

    Modified Code

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_sign - Returns the decision
+* @c: The value to decide on
+*
+* Return: It always returns success as zero
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
+}
