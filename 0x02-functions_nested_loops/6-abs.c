4704dcb6e8624b848b8ba46b80650beca30441f6
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-14 01:20:07 +0100

    Added New Code

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _abs - Returns the decision
+* @n: The value to decide on
+*
+* Return: It always returns success as zero
+*/
+
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else if (n == 0)
+{
+return (n);
+}
+else
+{
+return (n * -1);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,7 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
+int _abs(int);
+
 
 #endif
