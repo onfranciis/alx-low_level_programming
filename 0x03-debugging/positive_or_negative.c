d124579568d4b35a684f05e0dfdb324ad8b48970
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-15 22:27:44 +0100

    Added New Directory And First Project

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* positive_or_negative - Entry Function
+*
+* Return: It always returns success as zero
+*/
+void positive_or_negative(int n)
+{
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else
+{
+printf("You're on your own");
+}
+}
