a4ce4000ee7e4c6daeb9afa20ed144c88002f543
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-17 03:02:47 +0100

    0 Upload

---
+++ 0x04-more_functions_nested_loops/0-isuper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Main function
+ * @c: character to check
+ * Return: Either 0 or 1
+ */
+
+int _isupper(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
