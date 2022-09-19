facf292a03cc8c05393050a33ec569f9a35c820c
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-20 00:21:22 +0100

    Added 3 File

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - check the code
+ *
+ * @str: The string to be printed out
+ *
+ * Return:  None
+ */
+
+void _puts(char *str)
+{
+int x =0;
+while(*(str + x))
+{
+putchar(*(str + x));
+x++;
+}
+putchar('\n');
+}
