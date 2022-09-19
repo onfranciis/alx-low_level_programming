ccabac3eae4ed92144f58c1b7639f5da569c6d2b
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-20 00:45:22 +0100

    Betty Styled

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,13 +14,13 @@ void print_rev(char *s)
 { 
 int i = 0;
 
-while (*(s + i)) 
-i++;  
-i = i - 1; 
-while (i>=0) 
-{ 
-putchar (*(s+i)); 
-i--; 
-} 
-putchar ('\n'); 
+while (*(s + i))
+i++;
+i = i - 1;
+while (i>=0)
+{
+putchar (*(s+i));
+i--;
+}
+putchar ('\n');
 }
