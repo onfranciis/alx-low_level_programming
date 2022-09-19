f7f39a4a0ccb700e6c3bdd55964a2c4a48eca374
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-20 00:40:25 +0100

    Added 4 File

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0.
  */
-int putch(void)
+int main(void)
 {
     char *str;
 
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - check the code
+ *
+ * @s: The string to be printed out
+ *
+ * Return:  None
+ */
+
+void print_rev(char *s)
+{ 
+int i = 0;
+
+while (*(s + i)) 
+i++;  
+i = i - 1; 
+while (i>=0) 
+{ 
+_putchar (*(s+i)); 
+i--; 
+} 
+_putchar ('\n'); 
+}
