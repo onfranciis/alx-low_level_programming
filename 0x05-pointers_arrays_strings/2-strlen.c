fb5e2979cd9aac58cb114b9895d3f73b42c74c4a
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-09-19 22:55:08 +0100

    Added 2 File

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * swap_int - check the code
+ *
+ * @s: The first integer
+ *
+ * Return:  The number of characters in a string
+ */
+int _strlen(char *s)
+{
+    return strlen(s);
+}
